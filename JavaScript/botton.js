f5f83c751f62cb31df7fbb9c6e8605de20a50ced
武次優 <72009330+yutaketsugu@users.noreply.github.com>
2022-10-20 22:56:20 +0900

    Update botton.js

--- JavaScript/botton.js
+++ JavaScript/botton.js
@@ -1,8 +1,9 @@
-function OnOffButton(){
-    alert('JavaScriptが読み込まれました');
-}
-
 var aX = 0, aY = 0, aZ = 0;                     // 加速度の値を入れる変数を3個用意
+var flag = true;
+var cnt = 0;
+var arrX = new Array(40);
+var arrY = new Array(40);
+var arrZ = new Array(40);
  
 // 加速度センサの値が変化したら実行される devicemotion イベント
 window.addEventListener("devicemotion", (dat) => {
@@ -13,8 +14,14 @@ window.addEventListener("devicemotion", (dat) => {
  
 // 指定時間ごとに繰り返し実行される setInterval(実行する内容, 間隔[ms]) タイマーを設定
 var timer = window.setInterval(() => {
-    displayData();      // displayData 関数を実行
-}, 33); // 33msごとに（1秒間に約30回）
+    if(flag) {
+        displayData();      // displayData 関数を実行
+        arrX[i] = aX;
+        arrY[i] = aY;
+        arrZ[i] = aZ;
+        cnt++;
+    }
+}, 20); // 33msごとに（1秒間に約20回）
  
 // データを表示する displayData 関数
 function displayData() {
@@ -22,4 +29,14 @@ function displayData() {
     txt.innerHTML = "x: " + aX + "<br>"         // x軸の値
                   + "y: " + aY + "<br>"         // y軸の値
                   + "z: " + aZ;                 // z軸の値
+}
+
+for(var i = 0; i < arrX.length; i++) {
+    document.getElementById('list').appendChild(arrX[i]);
+}
+
+function OnOffButton(){
+    if(flag) {
+        flag = false;
+    }
 }
